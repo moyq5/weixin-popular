0e6746dc7677c821c87adec08dd86a97ed742f80
Moyq5 <mo_yq5@163.com>
2018-04-17 22:15:44 +0800

    修改gettoken接口为get请求方式

--- src/test/java/weixin/popular/test/TokenTest.java
+++ src/test/java/weixin/popular/test/TokenTest.java
@@ -7,11 +7,11 @@ import weixin.popular.bean.token.Token;
 
 public class TokenTest {
 
-	protected static String accessToken = "YhobE8Sr9ZBDL1wCp-QBBTjqtp4MfkJ2O06aHr0qtXxKIEecdlvBY1sCTmKe5mcg3Y9y3-iBlV144rxXuzR0FmX_nYCyJ-TuPB7ly197UUbVmDOoJ9FqUfDLPwZglrRGNFShAKDIAN";
+	protected static String accessToken = "DNWJ5Lf6NsohwKXqWiw9j6p6rh-0kjnD7dVrJ_LxeEOWQZ4g0n9O4T-HpYvZS5JLso44pmFUbxbUaCTslihbJgIanDUlzSx7nzv0HQf7JlEQNNaAFAVSS";
 	
 	@Test
 	public void token() {
-		Token token = TokenAPI.token("XXXXX", "XXXXX");
+		Token token = TokenAPI.token("wxb67653b0d21d5e5a", "ccb5236f56ecc8769cc84ac1c4b429df");
 		accessToken = token.getAccess_token();
 		System.out.println(token.getAccess_token());
 		// 请复制粘贴access_token 替换accessToken变量的值，方便其它接口测试类使用
